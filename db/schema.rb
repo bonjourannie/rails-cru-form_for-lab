a72dd7d20676920f2a937ec017c07ca3f303e786
Annie Truex <artruex@gmail.com>
2019-12-03 02:38:27 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -28,8 +28,8 @@ ActiveRecord::Schema.define(version: 20191203014515) do
   create_table "songs", force: :cascade do |t|
     t.string   "name"
     t.integer  "artist_id"
-    t.integer  "genre_id"
-    t.datetime "created_at", null: false
+    # t.integer  "genre_id"
+    # t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["artist_id"], name: "index_songs_on_artist_id"
     t.index ["genre_id"], name: "index_songs_on_genre_id"
