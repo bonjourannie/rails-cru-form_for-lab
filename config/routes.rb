e820055d878a26064ae8477fb3901f50411f47a7
Annie Truex <artruex@gmail.com>
2019-12-03 01:51:35 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'artists/index'
+
+  get 'artists/show'
+
+  get 'artists/new'
+
+  get 'artists/create'
+
+  get 'artists/edit'
+
+  get 'artists/update'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
