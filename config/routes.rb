8db04b36c077e421b2959c19f189687df4356c88
Annie Truex <artruex@gmail.com>
2019-12-03 02:46:08 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,43 @@
 Rails.application.routes.draw do
+  resources :songs
+  resources :genres
+  resources :artists
+  # get 'songs/index'
+
+  # get 'songs/show'
+
+  # get 'songs/new'
+
+  # get 'songs/create'
+
+  # get 'songs/edit'
+
+  # get 'songs/update'
+
+  # get 'genress/index'
+
+  # get 'genress/show'
+
+  # get 'genress/new'
+
+  # get 'genress/create'
+
+  # get 'genress/edit'
+
+  # get 'genress/update'
+
+  # get 'artists/index'
+
+  # get 'artists/show'
+
+  # get 'artists/new'
+
+  # get 'artists/create'
+
+  # get 'artists/edit'
+
+  # get 'artists/update'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
