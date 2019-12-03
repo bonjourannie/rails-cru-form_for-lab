7191953037196d2d529612a7e3d6823281794694
Annie Truex <artruex@gmail.com>
2019-12-03 02:03:06 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -8,12 +8,13 @@ class SongsController < ApplicationController
   end
 
   def new
-    @song = Song.new(song_params)
-	  @song.save
-	  redirect_to song_path(@song)
+    
   end
 
   def create
+    @song = Song.new(song_params)
+	  @song.save
+	  redirect_to song_path(@song)
   end
 
   def edit
