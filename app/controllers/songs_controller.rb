68b0454cc3f6675f58f635ccedf09e2d9fc02629
Annie Truex <artruex@gmail.com>
2019-12-03 02:04:15 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -22,6 +22,8 @@ class SongsController < ApplicationController
   end
 
   def update
-    
+    @song = Song.find(params[:id])
+	  @song.update(song_params)
+	  redirect_to song_path(@song)
   end
 end
